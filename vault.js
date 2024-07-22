20311ca2b8ee7f74293263e104d94430d7c128f7
Noah Bonham <nowh717@gmail.com>
2024-07-22 10:22:53 -0700

    new

---
+++ vault.js
@@ -0,0 +1,6 @@
+// first i assign each number to a name.
+let num1 = 50 / 5
+let num2 = 8 * 5
+let num3 = 27 + 12
+/*then i use the alert tag to create a pop up whenever the page is loaded into a broswer. i also added the "secret combination" at the end by using the variables.*/
+alert ("You have received this message because you have been chosen to open an important vault. Here is the secret combination:" + num1 + "-" + num2 + "-" + num3);
